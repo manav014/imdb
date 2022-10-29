08fc7cf7a27c48b0ade3d0476342cb7d2a893b8c
khu5h1 <khushirauniyar8@gmail.com>
2022-10-30 02:42:34 +0530

    Updated models.py

--- core/models.py
+++ core/models.py
@@ -22,6 +22,9 @@ class Author(models.Model):
 class Actor(models.Model):
     name = models.CharField(max_length=100)
 
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    
 class Movie_Author(models.Model):
     name = models.ForeignKey(Author, on_delete=models.CASCADE)
     mv = models.ForeignKey(Movie, on_delete=models.CASCADE)
